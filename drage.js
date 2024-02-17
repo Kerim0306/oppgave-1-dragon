38bff49a7ee71c896925d8588ae44e9910647f86
Kerim0306 <156143172+Kerim0306@users.noreply.github.com>
2024-02-17 18:28:01 +0100

    linked javascript

--- drage.js
+++ drage.js
@@ -1,7 +1,14 @@
-const characterNames = ["linda", "sara" "sofia" "Dracon"]
+const characterNames = ["linda", "sara", "sofia", "Dracon"]
 const characterHP = [100, 100, 100, 100]
 
-function attack(characternumber) {
+const p1 = document.querySelector("body > div:nth-child(1)")
+const p2 = document.querySelector("body > div:nth-child(2)")
+const p3 = document.querySelector("body > div:nth-child(3)")
+const drage1 = document.querySelector("body > div:nth-child(4)")
 
+// const HtmlElement
+
+function attack(characternumber) {
+alert(charaterName[characternumber])
 
 }
