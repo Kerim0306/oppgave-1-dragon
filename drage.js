1cb45574d87426ea0ed79d4bccf816e5336b0b79
Kerim0306 <156143172+Kerim0306@users.noreply.github.com>
2024-02-17 19:39:09 +0100

    damage done

--- drage.js
+++ drage.js
@@ -1,6 +1,6 @@
 const characterNames = ["linda", "sara", "sofia", "Dracon"]
 const characterDamage = [5, 7, 12, 20]
-const characterHP = [100, 100, 100, 100]
+const characterHP = [100, 100, 100, 200]
 
 const p1 = document.querySelector("body > div:nth-child(1)")
 const p2 = document.querySelector("body > div:nth-child(2)")
@@ -15,7 +15,7 @@ HtmlElements.forEach(setup)
 function setup(el, number) {
     if(number < 3){
         el.addEventListener("click", function(){attack (number)})
-        el.querySelector(".name").innertext = characterNames[number]
+        el.querySelector(".name").innerText = characterNames[number]
     }
 }
 
@@ -23,12 +23,16 @@ function attack(number) {
     characterHP[3] -= characterDamage[number]
     
     alert(characterNames[number] + " har gjort " + characterDamage [number] + " skade på " + characterNames[3])
+    HtmlElements[3].querySelector(".HP").innerText = characterHP[3] + "/200"
     
-let dragonTarget = Math.floor(Math.random() * 2)
-
+    let dragonTarget = Math.floor(Math.random() * 2)
+    
+    
+    
     characterHP[dragonTarget] -= characterDamage[3]
     alert(characterNames[3] + " har angrepet " + characterNames [dragonTarget]) 
-
+    
+    HtmlElements[dragonTarget].querySelector(".HP").innerText = characterHP[dragonTarget] + "/100"
 
 
 }
